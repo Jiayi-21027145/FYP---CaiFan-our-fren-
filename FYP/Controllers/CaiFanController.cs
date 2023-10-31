73111cff7de6592e70c0ae979f7abcbd4796827e
LEE JIA YI RP <21027145@myrp.edu.sg>
2023-10-31 15:46:08 +0800

    Add CaiFan controller and CaiFan.cshtml -> include necessary tab onto the website

---
+++ FYP/Controllers/CaiFanController.cs
@@ -0,0 +1,21 @@
+﻿using FYP.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FYP.Controllers;
+public class GiftShopController : Controller
+{
+    private readonly AppDbContext _dbCtx;
+
+    public GiftShopController(AppDbContext dbContext)
+    {
+        _dbCtx = dbContext;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
+
+
+
