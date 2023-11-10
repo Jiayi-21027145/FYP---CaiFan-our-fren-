37801b940e9b5b75dc2d08c6d154be911eac91f2
LEE JIA YI RP <21027145@myrp.edu.sg>
2023-11-10 23:52:16 +0800

    Updated

--- FYP5/Program.cs
+++ FYP5/Program.cs
@@ -1,47 +1,31 @@
 global using FYP5.Models;
-global using FYP5.Services;
-global using Microsoft.AspNetCore.Authentication;
-global using Microsoft.AspNetCore.Authentication.Cookies;
-global using Microsoft.AspNetCore.Authorization;
-global using Microsoft.AspNetCore.Mvc.Rendering;
-global using Microsoft.EntityFrameworkCore;
 global using RP.SOI.DotNet.Utils;
-global using RP.SOI.DotNet.Services;
-global using System.ComponentModel.DataAnnotations;
-global using System.Security.Claims;
 global using System.Data;
 
+using Microsoft.AspNetCore.Authentication.Cookies;
+
 var builder = WebApplication.CreateBuilder(args);
-
 builder.Services.AddControllersWithViews();
-builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-                .AddCookie(options =>
-                {
-                    options.LoginPath = "/Account/Login/";
-                    options.AccessDeniedPath = "/Account/Forbidden/";
-                });
-
-builder.Services.AddScoped<IDbService, DbService>();
-builder.Services.AddScoped<IAuthService, AuthService>();
-
-builder.Services.AddDbContext<AppDbContext>(
-   options => options.UseSqlServer(
-       builder.Configuration.GetConnectionString("DefaultConnection")));
-
-var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
+// authentication
+builder.Services
+.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+.AddCookie(options =>
 {
-    app.UseDeveloperExceptionPage();
+    options.LoginPath = "/Account/Login";
+    options.AccessDeniedPath = "/Account/Forbidden";
+});
+var app = builder.Build();
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
 }
 app.UseDefaultFiles();
 app.UseStaticFiles();
-app.UseRouting();
 app.UseAuthentication();
+app.UseRouting();
 app.UseAuthorization();
-
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
+name: "default",
+pattern: "{controller=Home}/{action=Index}/{id?}");
 app.Run();
