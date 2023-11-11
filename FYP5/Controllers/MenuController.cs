93ce7a3b0070a1e3089554cead7cbdf88dacd6b7
LEE JIA YI RP <21027145@myrp.edu.sg>
2023-11-11 14:34:43 +0800

    Add Menu view and controller

---
+++ FYP5/Controllers/MenuController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FYP5.Controllers
+{
+    public class MenuController : Controller
+    {
+        private readonly IWebHostEnvironment _env;
+
+        public MenuController(IWebHostEnvironment environment)
+        {
+            _env = environment;
+        }
+        public IActionResult Index()
+        {
+            DataTable dt = DBUtl.GetTable("SELECT * FROM Menu");
+            return View("Menu", dt.Rows);
+        }
+    }
+}
