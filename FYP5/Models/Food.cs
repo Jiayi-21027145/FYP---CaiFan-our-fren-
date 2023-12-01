e24b6ba2a4bd2762606f8780468f5a5b6eec5740
LEE JIA YI RP <21027145@myrp.edu.sg>
2023-12-01 23:02:56 +0800

    Add in new models - Dish, Food and summary

---
+++ FYP5/Models/Food.cs
@@ -0,0 +1,15 @@
+﻿namespace FYP5.Models
+{
+    public class Food
+    {
+        public string? UserId { get; set; }
+        public DateTime Date { get; set; }
+        public string? FoodOne { get; set; }
+        public string? FoodTwo { get; set; }
+        public string? FoodThree { get; set; }
+        public string? FoodFour { get; set; }
+        public string? FoodFive { get; set; }
+        public string? FoodSix { get; set; }
+
+    }
+}
