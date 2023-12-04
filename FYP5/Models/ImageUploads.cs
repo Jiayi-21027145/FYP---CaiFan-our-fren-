fc73ee27f16b4ca6fe065c54324e3104b118f226
Jia hui <21034587@myrp.edu.sg>
2023-12-04 10:07:00 +0800

    Update DishIden/Index.cshtml & Create Account/Update.cshtml

--- FYP5/Models/ImageUploads.cs
+++ FYP5/Models/ImageUploads.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace FYP5.Models;
 
@@ -8,14 +10,17 @@ public partial class ImageUploads
     public int UploadId { get; set; }
 
     public string? UserId { get; set; }
+    [Required(ErrorMessage = "Please enter Date/Time")]
+    [DataType(DataType.DateTime)]
+    [Remote(action: "VerifyDate", controller: "Performance")]
+    public DateTime ImageDt { get; set; }
+    public string ImageLc { get; set; } = null!;
 
     public string ImageName { get; set; } = null!;
 
     public byte[] ImageData { get; set; } = null!;
 
     public string ImageUrl { get; set; } = null!;
-
-    public DateTime? UploadDate { get; set; }
 
     public virtual JiakUser? User { get; set; }
 }
