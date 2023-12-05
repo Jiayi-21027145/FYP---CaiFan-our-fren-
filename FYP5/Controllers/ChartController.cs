41ce6f002ba475d8655a3ecfcd7cd281839457fa
xin hui tan <21018929@myrp.edu.sg>
2023-12-05 08:32:49 +0800

    commit

---
+++ FYP5/Controllers/ChartController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FYP5.Controllers
+{
+    public class ChartController : Controller
+    {
+        public IActionResult Line()
+        {
+            PrepareData(0);
+            ViewData["Chart"] = "line";
+            ViewData["Title"] = "Total Users Per Month";
+            ViewData["ShowLegend"] = "false";
+            return View("Chart");
+        }
+
+        private void PrepareData(int x)
+        {
+            int[] totalUserData = new int[] {0, 0, 0, 0, 0, 0, 0};
+            List<JiakUser> list = DBUtl.GetList<JiakUser>("SELECT * FROM JiakUser");
+
+            foreach (JiakUser user in list)
+            {
+
+            }
+        }
+    }
+}
